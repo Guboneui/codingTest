5f14819ea35024069c7cc707b0dd530ed0c7f971
starku2249 <starku2249@naver.com>
2020-12-09 02:58:07 +0900

    commit Message

---
+++ lv1_이상한 문자 만들기.py
@@ -0,0 +1,44 @@
+def solution(s):
+    s = s.lower()
+    k = s.split(" ")
+
+    kk = []
+
+    for word in k:
+        new_word = ""
+        for i in range(len(word)):
+            if i % 2 == 0:
+                new_word += word[i].upper()
+            else:
+                new_word += word[i].lower()
+        kk.append(new_word)
+
+    return " ".join(kk)
+
+
+"""
+def solution(s):
+    s = s.lower()
+    k = s.split()           //split 차이점 중요
+    
+    kk = []
+    
+    for word in k:
+        new_word = ""
+        for i in range(len(word)):
+            if i % 2 == 0:
+                new_word += word[i].upper()
+            else:
+                new_word += word[i].lower()
+        kk.append(new_word)
+        
+    return " ".join(kk)
+
+
+
+
+
+
+
+
+"""
