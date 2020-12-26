d883920189f052f337f121850ed3267622feda0b
starku2249 <starku2249@naver.com>
2020-12-27 05:05:12 +0900

    commit Message

---
+++ lv3_입국심사.py
@@ -0,0 +1,17 @@
+def solution(n, times):
+    answer = 0
+    start = 1
+    end = min(times) * n
+
+    while start <= end:
+        middle = (start + end) // 2
+        k = n
+        for i in times:
+            k = k - (middle // i)
+            if k <= 0:
+                answer = middle
+                end = middle - 1
+        if k > 0:
+            start = middle + 1
+
+    return answer
