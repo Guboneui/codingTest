fe80b530aa16bc83ee17d1c57f47e0c30bfd5732
starku2249 <starku2249@naver.com>
2020-12-27 20:19:40 +0900

    commit Message

---
+++ lv3_기둥과 보 설치.py
@@ -0,0 +1,32 @@
+def check(answer):
+    for x, y, cate in answer:
+        if cate == 0:   #기둥
+            if y==0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+                continue
+            else:
+                return False
+
+        elif cate == 1:     #보
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+                continue
+            else:
+                return False
+    return True
+
+
+def solution(n, build_frame):
+    answer = []
+    for frame in build_frame:
+        x, y, cate, ox = frame
+
+        if ox == 1:
+            answer.append([x, y, cate])
+            if check(answer) == False:
+                answer.remove([x, y, cate])
+
+        elif ox == 0:
+            answer.remove([x, y, cate])
+            if check(answer) == False:
+                answer.append(([x, y, cate]))
+
+    return answer.sort()
